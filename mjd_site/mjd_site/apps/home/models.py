b20ade9fc368350ae5bec0c4edb031c6a4057730
romanBelyanov <romkabel412@gmail.com>
2024-11-10 02:56:56 +0700

    Delete mjd_site directory

--- mjd_site/mjd_site/apps/home/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Signin(models.Model):
-    name = models.CharField(max_length=100)
-    region = models.CharField(max_length=200)
-    age = models.IntegerField()
-    floor = models.CharField(max_length=10)
-    password = models.CharField(max_length=100)
-    confirm_password = models.CharField(max_length=100)
